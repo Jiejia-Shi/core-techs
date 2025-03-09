a0aece18f70dfd2cae970c568520c5a20e3a2199
Jiejia-Shi <j279shi@uwaterloo.ca>
2025-03-09 16:10:01 -0400

    project initialization

---
+++ src/main/java/com/example/coretechs/CoreTechsApplication.java
@@ -0,0 +1,14 @@
+package com.example.coretechs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreTechsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreTechsApplication.class, args);
+        System.out.println("project starts successfully");
+    }
+
+}
---
+++ src/test/java/com/example/coretechs/CoreTechsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coretechs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreTechsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
